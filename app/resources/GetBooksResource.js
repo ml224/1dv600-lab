a8f3fd15edaa2a10faaca00855a587e9133c702f
Malin Lorch <ml224cv@student.lnu.se>
2017-02-03 10:27:38 -0700

    .

--- app/resources/GetBooksResource.js
+++ app/resources/GetBooksResource.js
@@ -3,38 +3,28 @@
 
     let LibraryDAO = require('../dao/LibraryDAO');
     let Book = require("../dao/Book");
-    let request = require("request");
+    //let request = require("request");
 
-    function listBooks(array) {
+
+
+    function addBooks(array) {
+
         let book1 = new Book("id", "title1", "author1", "genre1", "date1", "the first book in the list");
         let book2 = new Book("id2", "title2", "author2", "genre2", "date2", "the second book in the list");
         let book3 = new Book("id3", "title3", "author3", "genre3", "date3", "the third book in the list");
 
-        //adding book-objects to array
         array.push(book1, book2, book3);
+
+        return array;
+
     }
 
+    module.exports = function (callback){
+        let list = [];
+        addBooks(list);
 
-    module.exports = function(callback){
-        // The title is optional and is only present when searching. (You need yo modify the books.js file first)
-        request(function(error, response) {
-            //adding objects to array
-            let list = [];
-            listBooks(list);
-            let obj = {answer: list};
-
-           if(error) {
-               return callback(error);
-           }
-           if(response.statusCode !== 200) {
-               return callback(new Error("Error code, response not 200"));
-           }
-
-           return callback(null, console.log(JSON.stringify(obj)));
-        });
-
+        return callback(null, JSON.stringify({answer: list}));
     };
-
 }());
 
 
